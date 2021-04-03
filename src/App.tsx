351affe964f09f933350525b511af1166a8c7677
Robert Fuchs <robert.fuchs@logmein.com>
2021-04-03 21:27:45 +0200

    Apply color theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,58 @@
-import { ChakraProvider, Image, Flex, Box, Text, Icon, IconButton, Link } from '@chakra-ui/react';
+import { ChakraProvider, Image, Flex, Text, Icon, IconButton, Link } from '@chakra-ui/react';
 import ProfilePicture from './resources/images/robert-and-tinka.jpg';
 import { Linkedin, GitHub, Mail } from 'react-feather';
 
 function App() {
+  const fontColor = '#c7d5e0';
+  const backgroundColor = '#171a21';
+  const buttonVariant = 'unstyled';
+
   return (
     <ChakraProvider>
-      <Flex>
-        <Flex paddingX='3vw' pt='10vh' flexDirection='column' justifyContent='space-around'>
-          <Text fontSize='5xl' fontFamily='Bebas Neue' align='center' backgroundColor='#b0bec5' paddingY='1vh'>
+      <Flex backgroundColor={backgroundColor}>
+        <Flex width='60vw' paddingX='3vw' flexDirection='column' justifyContent='space-around'>
+          <Text fontSize='5xl' fontFamily='Bebas Neue' align='center' backgroundColor='#1b2838' color={fontColor} paddingY='1vh'>
             Robert Fuchs
           </Text>
-          <Text fontSize='6xl' fontFamily='Bebas Neue' align='center' paddingX='5vw'>
+          <Text fontSize='6xl' fontFamily='Bebas Neue' color={fontColor} align='center' paddingX='5vw'>
             Software Developer, Sports Enthusiast and Optimist
           </Text>
           <Flex flexDirection='row' justifyContent='space-evenly'>
             <IconButton
-              variant='ghost'
+              variant={buttonVariant}
               boxSize='5em' 
               icon={<Icon as={Linkedin} boxSize='5em' />}
               aria-label="Open LinkedIn Profile"
               href='https://www.linkedin.com/in/robert-f-0b87a218a'
               as={Link}
               isExternal={true}
+              color={fontColor}
             />
             <IconButton
-              variant='ghost'
+              variant={buttonVariant}
               boxSize='5em'
               icon={<Icon as={GitHub} boxSize='5em' />}
               aria-label="Open GitHub Profile"
               href='https://github.com/Treborium/robert-fuchs-codes'
               as={Link}
               isExternal={true}
+              color={fontColor}
             />
             <IconButton
-              variant='ghost'
+              variant={buttonVariant}
               boxSize='5em'
               icon={<Icon as={Mail} boxSize='5em' />}
               aria-label="Send E-Mail"
-              href='https://www.linkedin.com/in/robert-f-0b87a218a'
+              href='mailto:robert@fuchs-mail.com'
               as={Link}
               isExternal={true}
+              color={fontColor}
             />
           </Flex>
-          <Text> </Text>
         </Flex>
-        <Box>
-          <Image src={ProfilePicture} alt='Robert Fuchs with his dog' height='100vh' width='auto' objectFit='cover' fit='fill'/>
-        </Box>
+        <Flex width='40vw' alignItems='center' justifyContent='center' overflow='hidden'>
+          <Image src={ProfilePicture} alt='Robert Fuchs with his dog' height='100vh' objectFit='contain' fit='fill'/>
+        </Flex>
       </Flex>
     </ChakraProvider>
   );

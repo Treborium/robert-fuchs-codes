d9717221273d2a13e8219286f3d42c97d4a313f5
Robert Fuchs <robert.fuchs@logmein.com>
2021-04-06 16:38:45 +0200

    Add multiple sizes of the profile picture

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { ChakraProvider, Image, Flex, Text, Icon, IconButton, Link, Divider } from '@chakra-ui/react';
-import ProfilePicture from './resources/images/robert-and-tinka.jpg';
+import ProfilePictureSmall from './resources/images/robert-and-tinka-small.jpg';
+import ProfilePictureMedium from './resources/images/robert-and-tinka-medium.jpg';
+import ProfilePictureLarge from './resources/images/robert-and-tinka-large.jpg';
 import { Linkedin, GitHub, Mail } from 'react-feather';
 
 function App() {
@@ -12,7 +14,7 @@ function App() {
   return (
     <ChakraProvider>
       <Flex backgroundColor={backgroundColor} height='100vh' width='100vw'>
-        <Flex paddingX='3vw' flexDirection='column' justifyContent='space-around' alignItems='stretch' width='100vw'>
+        <Flex paddingX='3vw' flexDirection='column' justifyContent='space-around' alignItems='stretch'>
           <Text fontSize='3vw' fontFamily='Bebas Neue' align='center' backgroundColor={secondaryBackgroundColor} color={fontColor} paddingY='1vh'>
             Robert Fuchs
           </Text>
@@ -54,8 +56,8 @@ function App() {
             />
           </Flex>
         </Flex>
-        <Flex overflow='hidden' flexDirection='row-reverse' flexShrink={2}>
-          <Image src={ProfilePicture} alt='Robert Fuchs with his dog' objectFit='contain' fit='fill'/>
+        <Flex overflow='hidden' flexDirection='row-reverse'>
+          <Image src={ProfilePictureLarge} alt='Robert Fuchs with his dog' height='1080' width='auto' srcSet={`${ProfilePictureSmall} 267w, ${ProfilePictureMedium} 535w`}/>
         </Flex>
       </Flex>
     </ChakraProvider>

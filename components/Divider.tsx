a22a629ee820b0e46f7845b345985a258e6bc13a
Robert Fuchs <robert.fuchs@logmein.com>
2021-04-23 22:32:44 +0200

    Accept height prop for divider

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -1,5 +1,9 @@
 import { iconSize } from "./Theme";
 
-export default function Divider(): React.ReactElement {
-  return <div style={{ height: iconSize, borderLeft: `1px solid hsla(200, 10%, 50%, 100)` }}/>;
+interface DividerProps {
+  height: string;
+}
+
+export default function Divider({ height }: DividerProps): React.ReactElement {
+  return <div style={{ height: height, borderLeft: `1px solid hsla(200, 10%, 50%, 100)` }}/>;
 } 

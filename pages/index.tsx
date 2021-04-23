d97519d21ca38fe5c40ae45d6f4325cf78b0e08b
Robert Fuchs <robert.fuchs@logmein.com>
2021-04-23 22:33:34 +0200

    Display mobile or desktop layout depending on window size

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
-import DesktopApp from "../components/Desktop";
+import dynamic from 'next/dynamic'
+import { useMediaQuery } from '../components/Hooks';
+
+const DesktopApp = dynamic(() => import('../components/Desktop'));
+const MobileApp = dynamic(() => import('../components/Mobile'));
 
 export default function HomePage() {
-  return <DesktopApp />
+  const isTwoColumnMode = useMediaQuery('(min-width: 800px)');
+
+  return isTwoColumnMode ? <DesktopApp /> : <MobileApp />;
 }

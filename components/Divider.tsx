788ec2b8a2607aa996e08f40e96c9ef79709519e
Robert Fuchs <robert.fuchs@logmein.com>
2021-04-23 16:15:39 +0200

    Capitalize first letter of file

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -2,4 +2,4 @@ import { iconSize } from "./Theme";
 
 export default function Divider(): React.ReactElement {
   return <div style={{ height: iconSize, borderLeft: `1px solid hsla(200, 10%, 50%, 100)` }}/>;
-}
+} 

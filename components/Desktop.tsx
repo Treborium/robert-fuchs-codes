2674c8d109f90dc82e38ed1982a3ed6f17b0d151
Robert Fuchs <robert.fuchs@logmein.com>
2021-04-23 22:32:27 +0200

    Adjust styling of desktop layout

--- components/Desktop.tsx
+++ components/Desktop.tsx
@@ -10,19 +10,19 @@ export default function DesktopApp(): JSX.Element {
     <div style={{ display: 'flex', backgroundColor: backgroundColor, height: '100vh', width: '100vw', border: 'none' }}>
       <div style={{ display: 'flex', padding: '0 3vw', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'stretch', width: '60vw' }}>
 
-        <p style={{ fontSize: '4rem', fontFamily: 'Baloo Chettan 2', backgroundColor: secondaryBackgroundColor, color: fontColor, textAlign: 'center', padding: '1vh 0' }}>
+        <p style={{ fontSize: '3vw', fontFamily: 'Baloo Chettan 2', backgroundColor: secondaryBackgroundColor, color: fontColor, textAlign: 'center', padding: '1vh 0' }}>
           Robert Fuchs
         </p>
 
-        <p style={{ fontSize: '3rem', fontFamily: 'Baloo Chettan 2', color: fontColor, textAlign: 'center', padding: '0 5vw' }}>
+        <p style={{ fontSize: '2.5vw', fontFamily: 'Baloo Chettan 2', color: fontColor, textAlign: 'center', padding: '0 5vw' }}>
           Software Developer, Sports Enthusiast and Optimist
         </p>
 
         <div style={{ display: 'flex', justifyContent: 'space-evenly', alignItems: 'center' }}>
           <IconButton icon={<Linkedin color={fontColor} size={iconSize} strokeWidth='1px'/>} ariaLabel='Open LinkedIn Profile' href='https://www.linkedin.com/in/robert-f-0b87a218a' />
-          <Divider />
+          <Divider height={iconSize} />
           <IconButton icon={<GitHub color={fontColor} size={iconSize} strokeWidth='1px'/>} ariaLabel='Open GitHub Profile' href='https://github.com/Treborium/robert-fuchs-codes' />
-          <Divider />
+          <Divider height={iconSize} />
           <IconButton icon={<Mail color={fontColor} size={iconSize} strokeWidth='1px'/>} ariaLabel='Send E-Mail to Robert' href='mailto:robert@fuchs-mail.com' />
         </div>
       </div>

f9b09d0322541b92386817c4c0bf8a4e20801066
Robert Fuchs <robert.fuchs@logmein.com>
2021-09-25 00:06:03 +0200

    add hook for animations

---
+++ hooks/useAnimation.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export default function useAnimation(targetValue: number, durationInMilli: number, stepSize: number) {
+  const [progress, setProgress] = useState(0.0);
+  const animationUpdateIntervalInMilli = durationInMilli / (targetValue / stepSize);
+  console.log(animationUpdateIntervalInMilli);
+
+  useEffect(() => {
+    if (progress < targetValue) {
+      const timer = setInterval(() => {
+        setProgress((prevProgress) =>
+          prevProgress >= targetValue ? targetValue : prevProgress + stepSize
+        );
+      }, animationUpdateIntervalInMilli);
+
+      return () => {
+        clearInterval(timer);
+      };
+    }
+  }, []);
+
+  return progress;
+}

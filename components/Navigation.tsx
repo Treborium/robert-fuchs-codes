5aad22093802c7d26a92d746ab0a94529af85dc6
Robert Fuchs <robert.fuchs@logmein.com>
2021-09-24 20:19:36 +0200

    style navigation component

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { Box, Grid, Link, Typography } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 
 export default function Navigation() {
+  const linkStyle = {...styles.marginPerItem, ...styles.text};
+
   return (
-    <Box style={styles.container}>
-      <Link href="/home" style={styles.marginPerItem}>home</Link>
-      <Typography style={styles.marginPerItem}>|</Typography>
-      <Link href="/experience" style={styles.marginPerItem}>experience</Link>
-      <Typography style={styles.marginPerItem}>|</Typography>
-      <Link href="/projects" style={styles.marginPerItem}>projects</Link>
+    <Box sx={styles.container}>
+      <Link href="/home" sx={linkStyle}>home</Link>
+      <Typography sx={styles.marginPerItem}>|</Typography>
+      <Link href="/experience" sx={linkStyle}>experience</Link>
+      <Typography sx={styles.marginPerItem}>|</Typography>
+      <Link href="/projects" sx={linkStyle}>projects</Link>
     </Box>
   );
 }
@@ -16,9 +18,17 @@ export default function Navigation() {
 const styles = {
   container: {
     display: 'flex',
-    
+    position: 'fixed',
+    top: '35px',
+    left: '45px',
+    border: 'none',
   },
   marginPerItem: {
     margin: '0px 10px'
+  },
+  text: {
+    textTransform: 'uppercase',
+    letterSpacing: '0.1rem',
+    color: '#000000'
   }
-};
+} as const;

8ae4892680d03be2f9bc52630510f96c332e48df
Robert Fuchs <robert.fuchs@logmein.com>
2021-09-25 00:05:42 +0200

    create page component

---
+++ components/Page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container } from '@mui/material';
+
+import { theme } from './Theme';
+
+export interface Props {
+  children?: React.ReactNode;
+}
+
+export default function Page({children}: Props) {
+  return <Container maxWidth={false} disableGutters sx={styles.container} children={children} />;
+}
+
+const styles = {
+  container: {
+    display: 'flex',
+    backgroundColor: theme.backgroundColor,
+    height: '100vh',
+  },
+} as const;

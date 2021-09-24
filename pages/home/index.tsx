a65dee0da1652250f3587e707de477d0c8f1da54
Robert Fuchs <robert.fuchs@logmein.com>
2021-09-24 22:27:44 +0200

    display image on right half of home page

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,34 +1,30 @@
-import React from 'react';
-import { Box, Container, Typography, IconButton } from '@mui/material';
+import React, { useState } from 'react';
+import Image from 'next/image'
+import { Box, Container, Typography, Skeleton } from '@mui/material';
 
 import Navigation from '../../components/Navigation';
-import { GitHub, Linkedin, Mail } from 'react-feather';
+import SocialMediaButtonGroup from '../../components/SocialMediaButtonGroup';
 import { theme } from '../../components/Theme';
 
 export default function Home() {
+  const [isLoading, setLoading] = useState(true);
+
   return (
     <Container maxWidth={false} disableGutters sx={styles.container}>
       <Navigation />
       <Box sx={styles.leftHalf}>
-        <Typography variant='h1' sx={styles.heading} fontSize='48px'>
+        <Typography variant='h1' sx={styles.heading} fontSize='3rem'>
           Robert Fuchs
         </Typography>
-        <Typography variant='h2' fontSize='36px'>
+        <Typography variant='h2' fontSize='2rem'>
           Professional Software Developer, Sports Enthusiast and Optimist
         </Typography>
-        <Box sx={styles.icons}>
-          <IconButton>
-            <Linkedin strokeWidth='1px' color={theme.fontColor} size='80px'/>
-          </IconButton>
-          <IconButton>
-            <GitHub strokeWidth='1px' color={theme.fontColor} size='80px'/>
-          </IconButton>
-          <IconButton>
-            <Mail strokeWidth='1px' color={theme.fontColor} size='80px'/>
-          </IconButton>
-        </Box>
+        <SocialMediaButtonGroup />
       </Box>
-      <Box sx={styles.rightHalf}></Box>
+      <Box sx={styles.rightHalf}>
+        <Box sx={styles.gradiant} zIndex={1000}/>
+        <Image src='/title-picture.png' alt='picture of Robert Fuchs' layout='fill' objectFit='cover' placeholder='blur'/>
+      </Box>
     </Container>
   );
 }
@@ -42,6 +38,7 @@ const styles = {
     height: '100vh',
     width: '50%',
     paddingX: '45px',
+    paddingTop: '10vh',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-around',
@@ -49,17 +46,18 @@ const styles = {
   rightHalf: {
     height: '100vh',
     width: '50%',
+    position: 'relative',
   },
   heading: {
     textTransform: 'uppercase',
     letterSpacing: '0.2rem',
     fontWeight: 'bold',
   },
-  icons: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '100%'
-  },
-  subheading: {
-  },
+  gradiant: {
+    background: 'linear-gradient(90deg, #EFEFEF 0%, rgba(239, 239, 239, 0) 100%)',
+    mixBlendMode: 'normal',
+    width: '50%',
+    height: '100vh',
+    position: 'absolute'
+  }
 } as const;

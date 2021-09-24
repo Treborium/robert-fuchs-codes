accbe6b1bdba8dfd8332ffa04b9b498fd3f90b84
Robert Fuchs <robert.fuchs@logmein.com>
2021-09-25 00:05:26 +0200

    delete unused components

--- components/Divider.tsx
+++
@@ -1,9 +0,0 @@
-import { iconSize } from "./Theme";
-
-interface DividerProps {
-  height: string;
-}
-
-export default function Divider({ height }: DividerProps): React.ReactElement {
-  return <div style={{ height: height, borderLeft: `1px solid hsla(200, 10%, 50%, 100)` }}/>;
-} 

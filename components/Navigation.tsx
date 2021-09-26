2d2aa9be0cdddec831984d779f82424780a367ac
Robert Fuchs <robert.fuchs@logmein.com>
2021-09-26 21:16:18 +0200

    fix styling of Navigation component

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Box, Link, Typography } from '@mui/material';
 
+import { theme } from './Theme';
+
 export default function Navigation() {
+  const linkProps = {
+    color: theme.fontColor,
+  };
+
   return (
     <Box sx={styles.container}>
-      <Link href='/home' sx={styles.text}>
+      <Link href='/home' sx={styles.text} {...linkProps}>
         home
       </Link>
-      <Typography sx={styles.marginPerItem}>|</Typography>
-      <Link href='/experience' sx={styles.text}>
+      <Typography sx={styles.divider}>|</Typography>
+      <Link href='/experience' sx={styles.text} {...linkProps}>
         experience
       </Link>
-      <Typography sx={styles.marginPerItem}>|</Typography>
-      <Link href='/projects' sx={styles.text}>
+      <Typography sx={styles.divider}>|</Typography>
+      <Link href='/projects' sx={styles.text} {...linkProps}>
         projects
       </Link>
     </Box>
@@ -27,13 +33,15 @@ const styles = {
     left: '45px',
     border: 'none',
   },
-  marginPerItem: {
+  divider: {
     margin: '0px 10px',
+    fontSize: '1.5rem',
   },
   text: {
     textTransform: 'uppercase',
     letterSpacing: '0.1rem',
     color: '#000000',
     fontSize: '1.5rem',
+    
   },
 } as const;

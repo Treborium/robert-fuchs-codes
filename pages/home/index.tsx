3bb336c5fd1364a307f61d09c76c5bc6d01744c0
Robert Fuchs <robert.fuchs@logmein.com>
2021-10-11 18:26:30 +0200

    add mobile home page and conditionally render it

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,66 +1,11 @@
 import React from 'react';
-import Image from 'next/image';
-import { Box, Typography } from '@mui/material';
 
-import Navigation from '../../components/Navigation';
-import SocialMediaButtonGroup from '../../components/SocialMediaButtonGroup';
 import Page from '../../components/Page';
+import DesktopHome from '../../views/desktop/DesktopHome';
+import MobileHome from '../../views/mobile/MobileHome';
+import { useMediaQuery } from '../../hooks/useMediaQuery';
 
 export default function Home() {
-  return (
-    <Page>
-      <Navigation />
-      <Box sx={styles.leftHalf}>
-        <Typography variant='h1' sx={styles.heading} fontSize='3rem'>
-          Robert Fuchs
-        </Typography>
-        <Typography variant='h2' fontSize='2rem'>
-          Professional Software Developer, Sports Enthusiast and Optimist
-        </Typography>
-        <SocialMediaButtonGroup />
-      </Box>
-      <Box sx={styles.rightHalf}>
-        <Box sx={styles.gradiant} zIndex={1000} />
-        <Image
-          src='/title-picture.png'
-          alt='picture of Robert Fuchs'
-          layout='fill'
-          objectFit='cover'
-          placeholder='blur'
-          sizes='50vw'
-          priority
-        />
-      </Box>
-    </Page>
-  );
+  const isDesktop = useMediaQuery('(min-width: 800px)');
+  return <Page>{isDesktop ? <DesktopHome /> : <MobileHome />}</Page>;
 }
-
-const styles = {
-  leftHalf: {
-    height: '100vh',
-    width: '50%',
-    paddingX: '45px',
-    paddingTop: '10vh',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-  },
-  rightHalf: {
-    height: '100vh',
-    width: '50%',
-    position: 'relative',
-  },
-  heading: {
-    textTransform: 'uppercase',
-    letterSpacing: '0.2rem',
-    fontWeight: 'bold',
-  },
-  gradiant: {
-    background:
-      'linear-gradient(90deg, #EFEFEF 0%, rgba(239, 239, 239, 0) 100%)',
-    mixBlendMode: 'normal',
-    width: '50%',
-    height: '100vh',
-    position: 'absolute',
-  },
-} as const;
---
+++ views/mobile/MobileHome.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import Image from 'next/image';
+import { Box, Typography } from '@mui/material';
+
+import HamburgerMenu from '../../components/HamburgerMenu';
+import SocialMediaButtonGroup from '../../components/SocialMediaButtonGroup';
+
+export default function MobileHome() {
+  return (
+    <>
+      <HamburgerMenu />
+      <Box sx={styles.container}>
+        <Box sx={styles.topHalf}>
+          <Image
+            src='/title-picture.png'
+            alt='picture of Robert Fuchs'
+            layout='fill'
+            objectFit='cover'
+            placeholder='blur'
+            sizes='100vw'
+            priority
+          />
+          <Box sx={styles.gradiant} zIndex={999} />
+        </Box>
+        <Box sx={styles.bottomHalf}>
+          <Typography variant='h1' fontSize='2rem' sx={styles.heading}>
+            Robert Fuchs
+          </Typography>
+
+          <Typography variant='h2' fontSize='1.5rem' align='center'>
+            Professional Software Developer, Sports Enthusiast and Optimist
+          </Typography>
+
+          <SocialMediaButtonGroup iconSize='40px'/>
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  gradiant: {
+    background: 'linear-gradient(0deg, #EFEFEF 0%, rgba(239, 239, 239, 0) 50%)',
+    mixBlendMode: 'normal',
+    width: '100vw',
+    height: '50%',
+    position: 'fixed',
+  },
+  topHalf: {
+    height: '50%',
+    width: '100vw',
+    position: 'relative',
+  },
+  bottomHalf: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    height: '50%',
+    width: '100vw',
+    paddingX: '5vw',
+  },
+  heading: {
+    textTransform: 'uppercase',
+    letterSpacing: '0.2rem',
+    fontWeight: 'bold',
+  },
+} as const;

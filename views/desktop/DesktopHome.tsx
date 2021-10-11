be684ea7e737205563168454278a0cdb50811e1a
Robert Fuchs <robert.fuchs@logmein.com>
2021-10-11 18:10:28 +0200

    extract home page for desktop into own module

---
+++ views/desktop/DesktopHome.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import Image from 'next/image';
+import { Typography, Box } from '@mui/material';
+
+import SocialMediaButtonGroup from '../../components/SocialMediaButtonGroup';
+import Navigation from '../../components/Navigation';
+
+export default function DesktopHome() {
+  return (
+    <>
+      <Navigation />
+      <Box sx={styles.leftHalf}>
+        <Typography variant='h1' sx={styles.heading} fontSize='3rem'>
+          Robert Fuchs
+        </Typography>
+        <Typography variant='h2' fontSize='2rem'>
+          Professional Software Developer, Sports Enthusiast and Optimist
+        </Typography>
+        <SocialMediaButtonGroup />
+      </Box>
+      <Box sx={styles.rightHalf}>
+        <Box sx={styles.gradiant} zIndex={1000} />
+        <Image
+          src='/title-picture.png'
+          alt='picture of Robert Fuchs'
+          layout='fill'
+          objectFit='cover'
+          placeholder='blur'
+          sizes='50vw'
+          priority
+        />
+      </Box>
+    </>
+  );
+}
+
+const styles = {
+  leftHalf: {
+    height: '100vh',
+    width: '50%',
+    paddingX: '45px',
+    paddingTop: '10vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  rightHalf: {
+    height: '100vh',
+    width: '50%',
+    position: 'relative',
+  },
+  heading: {
+    textTransform: 'uppercase',
+    letterSpacing: '0.2rem',
+    fontWeight: 'bold',
+  },
+  gradiant: {
+    background:
+      'linear-gradient(90deg, #EFEFEF 0%, rgba(239, 239, 239, 0) 100%)',
+    mixBlendMode: 'normal',
+    width: '50%',
+    height: '100vh',
+    position: 'absolute',
+  },
+} as const;

740f3fe0dfdab6ce20158d61ffaf9e53b6c7151a
Robert Fuchs <robert.fuchs@logmein.com>
2022-02-07 12:18:27 +0100

    add experience components

--- pages/experience/index.tsx
+++ pages/experience/index.tsx
@@ -1,8 +1,46 @@
 import React from 'react';
 
-import { Typography } from '@mui/material';
+import { Box, Button, IconButton, Typography } from '@mui/material';
+import { CertificateOutline, SourceBranch, CodeTags } from 'mdi-material-ui';
 
 export default function Experience() {
-
-  return <Typography>Experience</Typography>
+  return (
+    <Box sx={styles.experiences}>
+      <Button sx={styles.button}>
+        <Box>
+          <CodeTags sx={styles.icon} />
+          <Typography align='center'>Skills</Typography>
+        </Box>
+      </Button>
+      <Button sx={styles.button}>
+        <Box>
+          <CertificateOutline sx={styles.icon} />
+          <Typography align='center'>Certificates</Typography>
+        </Box>
+      </Button>
+      <Button sx={styles.button}>
+        <Box>
+          <SourceBranch sx={styles.icon} />
+          <Typography align='center'>Resume</Typography>
+        </Box>
+      </Button>
+    </Box>
+  );
 }
+
+const styles = {
+  experiences: {
+    display: 'flex',
+    width: '100vw',
+    height: '100vh',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  button: {
+    color: 'white',
+  },
+  icon: {
+    height: '8vw',
+    width: '8vw',
+  },
+} as const;

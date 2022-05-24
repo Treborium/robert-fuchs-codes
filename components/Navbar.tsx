ead63d61ea05196a97d11060d37cd38a7c9d2c72
Robert Fuchs <robert.fuchs@futurice.com>
2022-05-24 15:53:29 +0200

    use a single shared layout

---
+++ components/Navbar.tsx
@@ -0,0 +1,7 @@
+export const Navbar: React.FC<{}> = ({ children }) => {
+  return (
+    <div
+      style={{ width: '100vw', height: '10vh', backgroundColor: 'black' }}
+    ></div>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { NextPage } from 'next';
+import { ReactElement, ReactNode } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <div style={{ margin: 0, padding: 0 }}>
+      {getLayout(<Component {...pageProps} />)}
+    </div>
+  );
 }
-
-export default MyApp
